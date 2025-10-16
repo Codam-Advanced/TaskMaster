7a31cbb2f1833f9eee27c93dbaf7ea5bdb24c282
Jisse Meruma <jissemeruma@gmail.com>
2025-10-16 21:18:21 +0200

    moved to the main function implemneted start time

--- taskmasterd/src/main.cpp
+++ taskmasterd/src/main.cpp
@@ -11,6 +11,41 @@
 
 using namespace taskmasterd;
 
+std::vector<Job> start()
+{
+    std::vector<Job> jobs;
+
+    try 
+    {
+        std::unordered_map<std::string, JobConfig> nodes = JobConfig::getJobConfigs("../tastconfig.yaml");
+
+        for (const auto& [name, config] : nodes) 
+        {
+            Job job(config);
+            if (config.autostart)
+            {
+                LOG_INFO("Autostarting job: " + name);
+                job.start();
+            }
+            jobs.push_back(std::move(job));
+        }
+    }
+    catch (const std::exception& e) 
+    {
+        LOG_FATAL("ERROR: Unable to start jobs reason: " + std::string(e.what()));
+    }
+
+    return jobs;
+}
+
+void stop(std::vector<Job>& jobs)
+{
+    for (auto& job : jobs) 
+    {
+        job.stop();
+    }
+}
+
 int main(int argc, char** argv)
 {
     (void)argc;
@@ -19,48 +54,19 @@ int main(int argc, char** argv)
     Logger::LogInterface::Initialize(PROGRAM_NAME, Logger::LogLevel::Debug, true);
     LOG_INFO("Starting " PROGRAM_NAME);
 
-   
+    EventManager::initialize();
 
-    try 
+    std::vector<Job> jobs = start();
+    stop(jobs);
+    
+
+    while (true) 
     {
-
-        std::unordered_map<std::string, JobConfig> nodes = JobConfig::getJobConfigs("../tastconfig.yaml");
-        EventManager::initialize();
-
-        for (const auto& [name, job] : nodes)
-        {
-            // print some debug info
-            LOG_DEBUG(("Loaded job config:" + name).c_str());
-            LOG_DEBUG(("Job name: " + job.name).c_str());
-            LOG_DEBUG(("Job cmd: " + job.cmd).c_str());
-            LOG_DEBUG(("Job working dir: " + job.working_dir).c_str());
-            LOG_DEBUG(("Job numprocs: " + std::to_string(job.numprocs)).c_str());
-            LOG_DEBUG(("Job umask: " + std::to_string(job.umask)).c_str());
-            LOG_DEBUG(("Job autostart: " + std::to_string(job.autostart)).c_str());
-            LOG_DEBUG(("Job exit codes: " + std::to_string(job.exit_codes[0])).c_str());
-            LOG_DEBUG(("job env: " + (job.env.find("STARTED_BY") != job.env.end() ? job.env.at("STARTED_BY") : "Empty")).c_str());
-        }
-
-        
-
-        Job job(nodes.begin()->second);
-        Job job2((++nodes.begin())->second);
-
-        job.start();
-        job2.start();
-        job2.stop();
-
-        while (true) 
-        {
-            EventManager::getInstance()->handleEvents();
-        }
-
-    }
-    catch (const std::exception& e) 
-    {
-        LOG_ERROR("Exception: " + std::string(e.what()));
-        return EXIT_FAILURE;
+        EventManager::getInstance()->handleEvents();
     }
 
     return 0;
 }
+
+
+
