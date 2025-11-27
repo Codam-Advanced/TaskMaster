14409b7ed9f4105d8366e556b59f1e3c084db7d4
rvan-mee <ruben.vanmeerten@hotmail.nl>
2025-11-27 16:56:17 +0100

    Fix(sockets) Fix closure of socket being written to resulting in an uncaught SIGPIPE, now send will return -1 and throw an exception

--- libs/ipc/include/ProtoWriter.hpp
+++ libs/ipc/include/ProtoWriter.hpp
@@ -18,18 +18,20 @@ namespace ipc
 template <typename T> class ProtoWriter
 {
 public:
-
     /**
-     * @brief Initializer function expecting a ProtoBuf class that has to be written to a file descriptor.
+     * @brief Initializer function expecting a ProtoBuf class that has to be written to a file
+     * descriptor.
      *
      * @param toWrite A ProtoBuf generated class that contains the SerializeToString member
      * function.
-     * @throw If serializing fails or if it has already been initialized a std::runtime_error will be thrown.
+     * @throw If serializing fails or if it has already been initialized a std::runtime_error will
+     * be thrown.
      */
     void init(T& toWrite)
     {
         if (_serializedMessage.size() > 0)
-            throw std::runtime_error("Already initialized this protoWriter, finish writing the message or reset");
+            throw std::runtime_error(
+                "Already initialized this protoWriter, finish writing the message or reset");
 
         if (!toWrite.SerializeToString(&_serializedMessage))
             throw std::runtime_error("Failed to serialize the message");
@@ -53,8 +55,9 @@ public:
         if (!_sizeWritten) {
             // Write the size of the message to the file descriptor.
             u32 size = htonl(_serializedMessage.size());
-            if (send(fd.getFd(), &size, sizeof(size), 0) == -1)
-                throw std::runtime_error("Failed to send the message size");
+            if (send(fd.getFd(), &size, sizeof(size), MSG_NOSIGNAL) == -1)
+                throw std::runtime_error(
+                    "Failed to send the message size, is the socket still open?");
             _sizeWritten = true;
             LOG_DEBUG("Successfully sent the message size")
             return false;
@@ -68,9 +71,10 @@ public:
             const char* data      = _serializedMessage.data() + _bytesWritten;
             isize       writeSize = std::min(_serializedMessage.size() - _bytesWritten,
                                        static_cast<size_t>(PROTOWRITER_MAX_WRITE_SIZE));
-            isize       bytesSent = send(fd.getFd(), data, writeSize, 0);
+            isize       bytesSent = send(fd.getFd(), data, writeSize, MSG_NOSIGNAL);
             if (bytesSent == -1)
-                throw std::runtime_error("Failed to send the command message");
+                throw std::runtime_error(
+                    "Failed to send the command message, is the socket still open?");
             _bytesWritten += bytesSent;
 
             if (static_cast<size_t>(_bytesWritten) == _serializedMessage.size()) {
@@ -82,7 +86,6 @@ public:
             LOG_DEBUG("Successfully sent " + std::to_string(bytesSent) + " amount of bytes")
             return false;
         }
-
     }
 
     /**
@@ -92,7 +95,7 @@ public:
     {
         _serializedMessage.clear();
         _bytesWritten = 0;
-        _sizeWritten = false;
+        _sizeWritten  = false;
     }
 
 private:
